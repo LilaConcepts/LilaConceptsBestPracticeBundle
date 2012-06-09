9fa23b06c3eb2fd83b9ba86e9d9732c4ddbd493d
Andre Lambert <andre@telegraafnet.nl>
2012-06-09 15:53:57 +0200

    initial checkin

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Lila\BestPracticeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('LilaBestPracticeBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ LilaBestPracticeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Lila\BestPracticeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LilaBestPracticeBundle extends Bundle
+{
+}
