5a7804f65223831f1a05b8feae55336be57ac8bc
Andre Lambert <afj.lambert@me.com>
2012-06-10 15:53:48 +0200

    wrong namespace fix

--- DependencyInjection/LilaBestPracticeExtension.php
+++ DependencyInjection/LilaBestPracticeExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Lila\BestPracticeBundle\DependencyInjection;
+namespace Lila\Bundle\BestPracticeBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
